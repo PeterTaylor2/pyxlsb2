559acc0b7251310a990340c3b0ae7c0732517f53
PeterTaylor2 <33470226+PeterTaylor2@users.noreply.github.com>
2026-07-26 18:55:00 +0100

    Update install-user.py
    add comments to install-user.py
    provide help options
    unless using the -y option then the user will be prompted before continuing with the copy

--- install-user.py
+++ install-user.py
@@ -1,3 +1,27 @@
+##########################################################################
+# This script installs the development version of pyxlsb2 package (from
+# the pyxlsb2 directory) to the so-called usersitepackages directory.
+#
+# usersitepackages is defined by site.getusersitepackages() function
+# and refers to a directory that is placed by python in the path before
+# the regular site-packages directory.
+#
+# Hence you can avoid all the issues of trying to prepare and install
+# packages via pip.
+#
+# The idea is that you copy the code into usersitepackages by running
+#            <python> install-user.py
+# where <python> represents your version of python however that is
+# determined in your particular development environment.
+#
+# Then you can edit and debug the code in usersitepackages using your
+# favourite python development platform until you have fixed any problems.
+#
+# After this process is complete then you can restore that version of
+# pyxlsb2 to your local directory by using the -r options:
+#            <python> install-user.py -r
+# and subsequently commit to source control.
+##########################################################################
 import os
 import site
 import glob
@@ -35,9 +59,14 @@ def copy_file(src, dst, verbose=False):
 
     return copy_source
 
-def main(src, dst, clean=False, verbose=False):
+def main(src, dst, clean=False, verbose=False, yes=False):
 
     print("copying from %s to %s (clean=%s)" % (src,dst,clean))
+    if not yes:
+        yes = input("Continue with the copy (Y/N=<CR>): ").upper().startswith("Y")
+        if not yes:
+            print_help()
+            return
     count = 0
 
     pattern = os.path.join(src, "*.py")
@@ -71,8 +100,22 @@ def main(src, dst, clean=False, verbose=False):
     if count > 0:
         print("%d file%s copied to %s" % (count, "s" if count > 1 else "", dst))
 
+def print_help():
+    print()
+    print("%s: installs the software from pyxlsb2 directory to user site-packages directory" % sys.argv[0])
+    print()
+    print("Before the copy you will be prompted whether you wish to continue unless you use the -y option")
+    print()
+    print("No command line arguments accepted - only options beginning with - or --")
+    print()
+    print("Options are as follows:")
+    print("   -c or --clean:   Clean the target directory")
+    print("   -v or --verbose: Print extra information")
+    print("   -r or --reverse: Copy from user site-packages to the pyxlsb2 directory")
+    print("   -y:              Don't prompt for whether you wish to continue")
+    print("   -h or --help:    Print this message and exit")
+
 if __name__ == "__main__":
-    import sys
     import getopt
 
     home = os.path.dirname(os.path.abspath(__file__))
@@ -80,8 +123,8 @@ if __name__ == "__main__":
     dst = get_target()
 
     try:
-        opts,args = getopt.getopt(sys.argv[1:], "vrcw", 
-                                  ["clean", "wait", "reverse", "verbose"])
+        opts,args = getopt.getopt(sys.argv[1:], "vrchy",
+                                  ["clean", "help", "reverse", "verbose"])
 
         if len(args) != 0:
             raise Exception("No parameters - only options")
@@ -93,13 +136,16 @@ if __name__ == "__main__":
                 kwargs["clean"] = True
             elif opt[0] == "-v" or opt[0] == "--verbose":
                 kwargs["verbose"] = True
-            elif opt[0] == "-w" or opt[0] == "--wait":
-                input("continue:")
+            elif opt[0] == "-h" or opt[0] == "--help":
+                print_help()
+                exit(0)
             elif opt[0] == "-r" or opt[0] == "--reverse":
                 reverse = True
+            elif opt[0] == "-y":
+                kwargs["yes"] = True
             else:
                 raise Exception("Unknown option %s=%s" % (opt[0], opt[1]))
-            
+
         if reverse:
             main(dst, src, **kwargs)
         else:
