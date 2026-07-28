2c3d91fac03a88ce4b2920593d04fa6cddff0b2e
PeterTaylor2 <33470226+PeterTaylor2@users.noreply.github.com>
2026-07-29 00:09:54 +0100

    Update workbook.py
    only add a SheetRecord to the Workbook if rec.name is not None
    - we found a case where an older excel sheet had a VERY_HIDDEN sheet with rec.name as None which in later code caused problems
    - also added exception handling in Workbook.get_sheet_by_name if the name could not be found

--- pyxlsb2/workbook.py
+++ pyxlsb2/workbook.py
@@ -54,13 +54,14 @@ class Workbook(object):
                 if rectype == rt.WB_PROP:
                     self.props = rec
                 elif rectype == rt.BUNDLE_SH:
-                    rec.type, rec.loc = self.get_sheet_info(workbook_rels, rec.rId)
-                    rec.id = sheet_counter
-                    sheet_counter +=1
-                    if rec.sheetId in self.sheet_index:
-                        raise Exception("sheet index %d for %s is already defined" % (rec.sheetId, rec))
-                    self.sheet_index[rec.sheetId] = rec
-                    self.sheets.append(rec)
+                    if rec.name is not None:
+                        rec.type, rec.loc = self.get_sheet_info(workbook_rels, rec.rId)
+                        rec.id = sheet_counter
+                        sheet_counter +=1
+                        if rec.sheetId in self.sheet_index:
+                            raise Exception("sheet index %d for %s is already defined" % (rec.sheetId, rec))
+                        self.sheet_index[rec.sheetId] = rec
+                        self.sheets.append(rec)
                 elif rectype == rt.BEGIN_EXTERNALS:
                     # https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-xlsb/e1620ef9-0809-478f-8c96-6a587bec09a4
                     self.externals = {'SupportingLinks':[], 'ExternalSheets': []}
@@ -171,15 +172,20 @@ class Workbook(object):
         Raises:
             IndexError: When the provided name is invalid.
         """
-        n = name.lower()
         loc = None
         target_sheet = None
+        found = False
+        n = name.lower()
         for sheet in self.sheets:
             if sheet.name.lower() == n:
                 loc = sheet.loc
                 target_sheet = sheet
+                found = True
                 break
-        
+
+        if not found:
+            raise IndexError("Invalid sheet name %s" % name)
+
         sheet_fname = loc[loc.rfind('/')+1:]
         if not sheet_fname:
             sheet_fname = loc[loc.rfind('\\')+1:]
